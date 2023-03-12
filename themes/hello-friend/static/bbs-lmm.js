035d1d90464ad70066369b076abc656e0d641bad
lillianwho <ciwiehenfan@sina.com>
2023-03-12 16:26:20 +0800

    vault backup: 2023-03-12 16:26:20

--- themes/hello-friend/static/bbs-lmm.js
+++ themes/hello-friend/static/bbs-lmm.js
@@ -1,30 +1,38 @@
 /*
-Last Modified time : 20221016 21:32 by https://immmmm.com
+Last Modified time : 20230302 21:32 by https://immmmm.com
 */
 const urls = [
-  {host:"https://me.edui.fun/",creatorId:"101",md5:"ba83fa02fc4b2ba621514941307e21be"},
-  {host:"https://me.edui.fun/",creatorId:"102",md5:"8faeaa1b58a25c03be347b5a7fb5b42a"},
-  {host:"https://bb.elizen.me/",creatorId:"101",md5:"f65df4d87240feb1cb247857a621a48f"},
-  {host:"https://memos.eallion.com/",creatorId:"101",md5:"171e4c30959e8c077a6c58b958624b31"},
-  {host:"https://qzone.boyhu.cn/",creatorId:"101",md5:"55be217893c75baf8571837197de4a3e"},
-  {host:"https://me.chenplus.com/",creatorId:"101",md5:"d1ed9c15ba6d0cb18464118c6288a8ff"},
-  {host:"https://memos.life97.top/",creatorId:"101",md5:"d41d8cd98f00b204e9800998ecf8427e"},
-  {host:"https://memos.1900.live/",creatorId:"101",md5:"cc38267b10cc25dfc62209f8ca34589e"},
-  {host:"https://memos.m1s.one/",creatorId:"101",md5:"c0a3e4fce8d8ae0651fd0b1ed717d186"},
+  {host:"https://me.edui.fun/",creatorId:"101",imgsrc:"https://cdn.sep.cc/avatar/ba83fa02fc4b2ba621514941307e21be"},
+  {host:"https://memos.eallion.com/",creatorId:"101",imgsrc:"https://cdn.sep.cc/avatar/171e4c30959e8c077a6c58b958624b31"},
+  {host:"https://isay.live/",creatorId:"101",imgsrc:"https://cdn.sep.cc/avatar/f0e33f4d097fe2e9fd74b9b129e7a655"},
+  {host:"https://memos.noionion.cn/",creatorId:"1",imgsrc:"https://cdn.sep.cc/avatar/d240a727ea154be465796ef2644aac16"},
+  {host:"https://qzone.boyhu.cn/",creatorId:"101",imgsrc:"https://cdn.sep.cc/avatar/55be217893c75baf8571837197de4a3e"},
+  {host:"https://me.chenplus.com/",creatorId:"101",imgsrc:"https://chenyyds.com/avatar.png"},
+  {host:"https://memos.life97.top/",creatorId:"101",imgsrc:"https://cdn.sep.cc/avatar/d41d8cd98f00b204e9800998ecf8427e"},
+  {host:"https://memos.1900.live/",creatorId:"101",imgsrc:"https://cdn.sep.cc/avatar/cc38267b10cc25dfc62209f8ca34589e"},
+  {host:"https://bb.seersu.top/",creatorId:"101",imgsrc:"https://cdn.sep.cc/avatar/84b712148a63b44dd97ede997bc3efa5"},
+  {host:"https://memo.wananaiko.com/",creatorId:"1",imgsrc:"https://thirdqq.qlogo.cn/g?b=sdk&k=7AFd0gic7wVsNg2ToQrQU2w&s=140&t=1583250600"},
+  {host:"https://memos.skyue.com/",creatorId:"1",imgsrc:"https://cdn.sep.cc/avatar/c3fb4bb4d5101284ddd672fb722cdd7d"},
+  {host:"https://memos.koobai.com/",creatorId:"1",imgsrc:"https://cdn.sep.cc/avatar/3b3d336a7d389b7ae8531cbe177ae9b7"},
+  {host:"https://dusays-memos.itisn.cyou/",creatorId:"1",imgsrc:"https://cdn.sep.cc/avatar/28b57baa4e8f13fe4292ccb2de267e30"},
+  {host:"https://api.mm.xlap.top/",creatorId:"1",imgsrc:"https://cdn.sep.cc/avatar/9855359e235402ea030ccd7aad041164"},
+  {host:"https://memos.vlieo.com/",creatorId:"1",imgsrc:"https://cdn.sep.cc/avatar/d36125dcbca7fde4200fd76f6aa1fbe9"},
+  {host:"https://flomo.010316.xyz/",creatorId:"1",imgsrc:"https://cdn.sep.cc/avatar/81f9378195d84e8ff73aad9ded2b9808"},
+  {host:"https://memos.wiki-power.com/",creatorId:"1",imgsrc:"https://cdn.sep.cc/avatar/44218020ee434de43516c19f05c71593"}
 ]
 
 var bbDom = document.querySelector('#bbs');
 var load = '<div id="load" onclick="nextFetch()" ><button class="load-btn button-load">加载更多</button></div>'
 var loading = '<div class="loader"><svg class="circular" viewBox="25 25 50 50"><circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/></svg></div>'
 var bbsDatas = [],bbsData = {},nextDatas = [],nextData = {},limit = 2
-var page = 1,offset = 0,nextLength = 0,nextDom='',bbUrlNow = '',md5Now = '',hostNow = '',creIdNow = ''
+var page = 1,offset = 0,nextLength = 0,nextDom='',bbUrlNow = '',imgsrcNow = '',hostNow = '',creIdNow = ''
 
 bbDom.innerHTML = loading
 allUrls()
 function allUrls(){
   var myHtml = ''
   for(var i=0;i < urls.length;i++){
-    myHtml += '<div class="bbs-urls " onclick="urlsNow(this)" data-host="'+urls[i].host+'" data-creatorId="'+urls[i].creatorId+'" data-md5="'+urls[i].md5+'" data-index="'+i+'"><img src="https://cravatar.cn/avatar/'+urls[i].md5+'" alt=""></div>'
+    myHtml += '<div class="bbs-urls " onclick="urlsNow(this)" data-host="'+urls[i].host+'" data-creatorId="'+urls[i].creatorId+'" data-imgsrc="'+urls[i].imgsrc+'" data-index="'+i+'"><img src="'+urls[i].imgsrc+'" alt=""></div>'
   }
   myHtml += '<div class="bbs-urls urls-button" onclick="urlsNow(this)" data-type="random"><svg t="1665928089691" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2562" width="32" height="32"><path d="M913.2 672l98.8 57.1c5.3 3.1 5.3 10.8 0 13.9l-43.4 25L710.4 924c-2.7 1.5-6-0.4-6-3.5V772c0-2.2-1.8-4-4-4H544c-70.4 0-134.4-28.8-180.8-75.2-11.1-11.1-21.2-23.2-30.1-36.1-6.4-9.2-20-9.1-26.4 0.1C260.5 723.9 183.1 768 96 768h-48c-26.5 0-48-21.5-48-48s21.5-48 48-48h48c42.5 0 82.6-16.7 112.9-47.1 30.4-30.4 47.1-70.5 47.1-112.9s-16.7-82.6-47.1-112.9C178.6 368.7 138.4 352 96 352h-48c-26.5 0-48-21.5-48-48s21.5-48 48-48h48c70.4 0 134.4 28.8 180.8 75.2 11.1 11.1 21.2 23.2 30.1 36.1 6.4 9.2 20 9.1 26.4-0.1 46.3-67 123.6-111.1 210.8-111.1H700.4c2.2 0 4-1.8 4-4V103.4c0-3.1 3.3-5 6-3.5l258.2 156 43.4 25.1c5.3 3.1 5.3 10.8 0 13.9L913.2 352 710.4 476c-2.7 1.5-6-0.4-6-3.5V356c0-2.2-1.8-4-4-4H544c-42.5 0-82.6 16.7-112.9 47.1-30.4 30.4-47.1 70.5-47.1 112.9 0 42.5 16.7 82.6 47.1 112.9C461.4 655.3 501.5 672 544 672H700.4c2.2 0 4-1.8 4-4V551.4c0-3.1 3.3-5 6-3.5L913.2 672z" p-id="2563" fill="#f5f5f5"></path></svg></div>'
   myHtml += '<div class="bbs-urls urls-button"><a href="https://immmmm.com/bbs-by-memos/"><svg t="1665929410343" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6308" width="32" height="32"><path d="M906.212134 565.732986 565.732986 565.732986 565.732986 906.212134C565.732986 926.013685 541.666486 959.972 511.97312 959.972 482.297674 959.972 458.213254 926.013685 458.213254 906.212134L458.213254 565.732986 117.734106 565.732986C97.950475 565.732986 63.97424 541.666486 63.97424 511.97312 63.97424 482.279754 97.950475 458.213254 117.734106 458.213254L458.213254 458.213254 458.213254 117.734106C458.213254 97.950475 482.297674 63.97424 511.97312 63.97424 541.666486 63.97424 565.732986 97.950475 565.732986 117.734106L565.732986 458.213254 906.212134 458.213254C925.995765 458.213254 959.972 482.279754 959.972 511.97312 959.972 541.666486 925.995765 565.732986 906.212134 565.732986Z" p-id="6309" fill="#f5f5f5"></path></svg></a></div>'
@@ -58,13 +66,13 @@ function urlsNow(e){
       var num = Math.round(Math.random() * (urls.length-1))
       hostNow = urls[num].host
       creIdNow = urls[num].creatorId
-      md5Now =urls[num].md5
+      imgsrcNow =urls[num].imgsrc
       domUrls[num].classList.add("url-now")
     }else{
       domUrls[e.getAttribute("data-index")].classList.add("url-now")
       hostNow = e.getAttribute("data-host")
       creIdNow = e.getAttribute("data-creatorId")
-      md5Now = e.getAttribute("data-md5")
+      imgsrcNow = e.getAttribute("data-imgsrc")
     }
     bbUrlNow = hostNow+"api/memo?creatorId="+creIdNow+"&rowStatus=NORMAL&limit=10"
     fetch(bbUrlNow).then(res => res.json()).then( resdata =>{
@@ -76,8 +84,8 @@ function urlsNow(e){
             bbsData = {
               updatedTs: resValue.updatedTs,
               creatorId:resValue.creatorId,
-              creator: resValue.creator.name,
-              mailmd5: md5Now,
+              creator: resValue.creatorName || resValue.creator.nickname || resValue.creator.name,
+              imgsrc: imgsrcNow,
               content: resValue.content,
               resourceList: resValue.resourceList,
               url:hostNow
@@ -117,8 +125,8 @@ function getNextList(){
       nextData = {
         updatedTs: resValue.updatedTs,
         creatorId:resValue.creatorId,
-        creator: resValue.creator.name,
-        mailmd5: md5Now,
+        creator: resValue.creatorName || resValue.creator.nickname || resValue.creator.name,
+        imgsrc: imgsrcNow,
         content: resValue.content,
         resourceList: resValue.resourceList,
         url:hostNow
@@ -139,8 +147,8 @@ const withTimeout = (millis, promise) => {
 const fetchBBser = async () => {
   const results = await Promise.allSettled(urls.map(
     //限时
-    //url => withTimeout(2000,fetch(url.host+"api/memo?creatorId="+url.creatorId+"&rowStatus=NORMAL&limit="+limit).then(response => response.json()).then(resdata => resdata.data))
-    url => fetch(url.host+"api/memo?creatorId="+url.creatorId+"&rowStatus=NORMAL&limit="+limit).then(response => response.json()).then(resdata => resdata.data)
+    url => withTimeout(2000,fetch(url.host+"api/memo?creatorId="+url.creatorId+"&rowStatus=NORMAL&limit="+limit).then(response => response.json()).then(resdata => resdata.data))
+    //url => fetch(url.host+"api/memo?creatorId="+url.creatorId+"&rowStatus=NORMAL&limit="+limit).then(response => response.json()).then(resdata => resdata.data)
   )).then(results=> {
     //console.log(results)
     bbDom.innerHTML = ''
@@ -153,8 +161,8 @@ const fetchBBser = async () => {
           bbsData = {
             updatedTs: resValue.updatedTs,
             creatorId:resValue.creatorId,
-            creator: resValue.creator.name,
-            mailmd5: urls[i].md5,
+            creator: resValue.creatorName || resValue.creator.nickname || resValue.creator.name,
+            imgsrc: urls[i].imgsrc,
             content: resValue.content,
             resourceList: resValue.resourceList,
             url:urls[i].host
@@ -185,51 +193,33 @@ function uniqueFunc(arr){
 // 插入 html 
 function updateHTMl(data){
   var result="",resultAll="";
-  const CODE_BLOCK_REG = /<p>```<\/p>(\S*?)\s([\s\S]*?)<p>```<\/p>(\n?)/g;
-  const TODO_LIST_REG = /- \[ \] ([\S ]+)(\n?)/g;
-  const DONE_LIST_REG = /- \[x\] ([\S ]+)(\n?)/g;
-  const ORDERED_LIST_REG = /(\d+)\. ([\S ]+)(\n?)/g;
-  const UNORDERED_LIST_REG = /[*-] ([\S ]+)(\n?)/g;
-  const PARAGRAPH_REG = /^([\S ]*)(\n?)/mg;
-  const TAG_REG = /#([^\s#]+?) /g;
-  const IMAGE_OLD_REG = /!\[.*?\]\(\/([a-z]\/[a-z]\/.+?)\)/g;
-  const IMAGE_REG = /!\[.*?\]\((.+?)\)/g;
-  const LINK_REG = /\[(.*?)\]\((.+?)\)/g;
-  const LINK_BILIBILI_REG = /\[(.*?)\]\((.+?)\)/g;
-  const MARK_REG = /@\[([\S ]+?)\]\((\S+?)\)/g;
-  const BOLD_REG = /\*\*([\S ]+)\*\*/g;
-  const EMPHASIS_REG = /\*([\S ]+)\*/g;
-  const PLAIN_LINK_REG = /((ht|f)tps?:\/\/[\w\-]+\.[\w\-]+[\w\-\.,@?^=%&:\/~\+#]*[\w\-\@?^=%&\/~\+#]) /g;
-  const INLINE_CODE_REG = /`([\S ]+?)`/g;
-  const PLAIN_TEXT_REG = /([\S ]+)/g;
-
-  const QUOTE_REG = /> ([\S ]+)/mg;
-  const MARK_IMG_REG = /^(.*)(\n\!\[)/;
+  const TAG_REG = /#([^\s#]+?) /g
+  , BILIBILI_REG = /<a.*?href="https:\/\/www\.bilibili\.com\/video\/((av[\d]{1,10})|(BV([\w]{10})))\/?".*?>.*<\/a>/g
+  , NETEASE_MUSIC_REG = /<a.*?href="https:\/\/music\.163\.com\/.*id=([0-9]+)".*?>.*<\/a>/g
+  , QQMUSIC_REG = /<a.*?href="https\:\/\/y\.qq\.com\/.*(\/[0-9a-zA-Z]+)(\.html)?".*?>.*?<\/a>/g
+  , QQVIDEO_REG = /<a.*?href="https:\/\/v\.qq\.com\/.*\/([a-z|A-Z|0-9]+)\.html".*?>.*<\/a>/g
+  , YOUKU_REG = /<a.*?href="https:\/\/v\.youku\.com\/.*\/id_([a-z|A-Z|0-9|==]+)\.html".*?>.*<\/a>/g
+  , YOUTUBE_REG = /<a.*?href="https:\/\/www\.youtube\.com\/watch\?v\=([a-z|A-Z|0-9]{11})\".*?>.*<\/a>/g;
+  marked.setOptions({
+    breaks: true,
+    smartypants: true,
+    langPrefix: 'language-'
+  });
 
   for(var i=0;i < data.length;i++){
       //console.log(data[i].content)
       var memos = data[i].url
       var bbContREG = data[i].content
-        .replace(/([\u4e00-\u9fa5])([A-Za-z0-9?.,;[\]]+)/g, "$1 $2")
-        .replace(/([A-Za-z0-9?.,;[\]]+)([\u4e00-\u9fa5])/g, "$1 $2")
-        .replace(TODO_LIST_REG, "<p><span class='todo-block todo' data-value='TODO'></span>$1</p>$2")
-        .replace(DONE_LIST_REG, "<p><span class='todo-block done' data-value='DONE'>✓</span>$1</p>$2")
-        .replace(ORDERED_LIST_REG, "<span class='ol-block'>$1.</span>$2 $3")
-        .replace(UNORDERED_LIST_REG, "<span class='ul-block'>•</span>$1 $2")
-        .replace(QUOTE_REG, "<blockquote>$1</blockquote>")
-        .replace(PARAGRAPH_REG, "<p>$1</p>$2")
-        .replace(MARK_IMG_REG, "<p>$1</p>$2")
-        .replace(CODE_BLOCK_REG, "<pre lang='$1'>\n$2</pre>$3")
-        .replace(IMAGE_OLD_REG, "<img class='img old square' src='"+memos+"$1' />")
-        .replace(IMAGE_REG, "<img class='img square' src='$1' />")
-        .replace(MARK_REG, "<span class='memo-link-text' data-value='$2'>$1</span>")
-        .replace(BOLD_REG, "<strong>$1</strong>")
-        .replace(EMPHASIS_REG, "<em>$1</em>")
-        .replace(LINK_REG, "<a class='link' target='_blank' rel='noreferrer' href='$2'>$1</a>")
-        .replace(INLINE_CODE_REG, "<code>$1</code>")
-        .replace(PLAIN_LINK_REG, "<a class='link' target='_blank' rel='noreferrer' href='$1'>$1</a> ")
         .replace(TAG_REG, "<span class='tag-span'>#$1</span> ")
-        .replace(PLAIN_TEXT_REG, "$1")
+        
+      bbContREG = marked.parse(bbContREG)
+        .replace(BILIBILI_REG, "<div class='video-wrapper'><iframe src='//player.bilibili.com/player.html?bvid=$1&as_wide=1&high_quality=1&danmaku=0' scrolling='no' border='0' frameborder='no' framespacing='0' allowfullscreen='true'></iframe></div>")
+        .replace(NETEASE_MUSIC_REG, "<meting-js auto='https://music.163.com/#/song?id=$1'></meting-js>")
+        .replace(QQMUSIC_REG, "<meting-js auto='https://y.qq.com/n/yqq/song$1.html'></meting-js>")
+        .replace(QQVIDEO_REG, "<div class='video-wrapper'><iframe src='//v.qq.com/iframe/player.html?vid=$1' allowFullScreen='true' frameborder='no'></iframe></div>")
+        .replace(YOUKU_REG, "<div class='video-wrapper'><iframe src='https://player.youku.com/embed/$1' frameborder=0 'allowfullscreen'></iframe></div>")
+        .replace(YOUTUBE_REG, "<div class='video-wrapper'><iframe src='https://www.youtube.com/embed/$1' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen title='YouTube Video'></iframe></div>")
+        
       //console.log(bbContREG)
       //解析内置资源文件
       if(data[i].resourceList && data[i].resourceList.length > 0){
@@ -237,12 +227,19 @@ function updateHTMl(data){
         var imgUrl='',resUrl='',resImgLength = 0;
         for(var j=0;j < resourceList.length;j++){
           var restype = resourceList[j].type.slice(0,5);
+          var resexlink = resourceList[j].externalLink
+          var resLink = ''
+          if(resexlink){
+            resLink = resexlink
+          }else{
+            resLink = memos+'o/r/'+resourceList[j].id+'/'+resourceList[j].filename
+          }
           if(restype == 'image'){
-            imgUrl += '<figure class="gallery-thumbnail"><img class="img thumbnail-image" src="'+memos+'o/r/'+resourceList[j].id+'/'+resourceList[j].filename+'"/></figure>'
+            imgUrl += '<figure class="gallery-thumbnail"><img class="img thumbnail-image" src="'+resLink+'"/></figure>'
             resImgLength = resImgLength + 1 
           }
-          if(restype !== 'image' && restype !== 'video'){
-            resUrl += '<a target="_blank" rel="noreferrer" href="'+memos+'o/r/'+resourceList[j].id+'/'+resourceList[j].filename+'">'+resourceList[j].filename+'</a>'
+          if(restype !== 'image'){
+            resUrl += '<a target="_blank" rel="noreferrer" href="'+resLink+'">'+resourceList[j].filename+'</a>'
           }
         }
         if(imgUrl){
@@ -254,7 +251,7 @@ function updateHTMl(data){
           bbContREG += '<div class="resour">'+resUrl+'</div>'
         }
       }
-      result += '<li class=""><div class="bbs-avatar"><img src="https://cravatar.cn/avatar/'+data[i].mailmd5+'" alt=""><a href="'+data[i].url+'u/'+data[i].creatorId+'" target="_blank" rel="noopener noreferrer" class="bbs-creator">'+data[i].creator+'</a><span class="bbs-dot">·</span><span class="bbs-date">'+new Date(data[i].updatedTs * 1000).toLocaleString()+'</span></div><div class="bbs-content"><div class="bbs-text">'+bbContREG+'</div></div></li>'
+      result += '<li class=""><div class="bbs-avatar"><img src="'+data[i].imgsrc+'" alt=""><a href="'+data[i].url+'u/'+data[i].creatorId+'" target="_blank" rel="noopener noreferrer" class="bbs-creator">'+data[i].creator+'</a><span class="bbs-dot">·</span><span class="bbs-date">'+new Date(data[i].updatedTs * 1000).toLocaleString()+'</span></div><div class="bbs-content"><div class="bbs-text">'+bbContREG+'</div></div></li>'
   }// end for
   var bbBefore = "<section class='bbs-timeline'><ul class='list'>"
   var bbAfter = "</ul></section>"
